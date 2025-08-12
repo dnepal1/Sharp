b73d22f5e45cf38828e3d7e934e06106e1f04f80
dnepal1 <leozodiacs420@gmail.com>
2025-08-12 10:33:54 +1000

    labCode
    Some of the code from my previous lab

---
+++ mutiplicationtable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class MultiplicationTable
+{
+    static void Main()
+    {
+        Console.Write("Enter a number to display its multiplication table: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"Multiplication Table for {num}:");
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine($"{num} x {i} = {num * i}");
+        }
+    }
+}
---
+++ namendage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HelloWorldApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the C# program in Visual Studio Code!");
+
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter your age: ");
+            int age;
+            while (!int.TryParse(Console.ReadLine(), out age))
+            {
+                Console.Write("Invalid input. Please enter a number for age: ");
+            }
+
+            Console.WriteLine($"\nHello {name}, you are {age} years old.");
+        }
+    }
+}
